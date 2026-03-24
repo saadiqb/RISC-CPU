1fe038a77b28ef12ee21102613040168074c25d9
Saad Rizvi <kazegyjhon@gmail.com>
2026-03-24 00:36:29 -0400

    Fixing ram.v bug

--- HDL/ram.v
+++ HDL/ram.v
@@ -4,29 +4,26 @@ module ram (
     input wire write,
     input wire [8:0] address,
     input wire [31:0] data_in,
-    output reg [31:0] data_out
+    output wire [31:0] data_out
 );
     // 512 x 32-bit memory array
     reg [31:0] memory [0:511]; 
 
-    integer i; 
-
-    // Initialize all memory to 0 to avoid undefined 'x' states in simulation
+    integer i;
     initial begin
         for (i = 0; i < 512; i = i + 1) begin
             memory[i] = 32'b0;
         end
     end
 
-    // Synchronous Read and Write
+    // Synchronous Write
     always @(posedge clk) begin
-        // Write operation
         if (write) begin
             memory[address] <= data_in;
         end
-        // Read operation
-        if (read) begin
-            data_out <= memory[address];
-        end
     end
+
+    // Asynchronous Read
+    assign data_out = read ? memory[address] : 32'b0;
+
 endmodule
