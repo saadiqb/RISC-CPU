5155a1e51ff062e22276283363b65d0fe7da3c40
Saad Rizvi <kazegyjhon@gmail.com>
2026-03-31 10:40:14 -0400

    fixing test bench hex codes

--- sim/Phase3_tb.v
+++ sim/Phase3_tb.v
@@ -42,14 +42,12 @@ module Phase3_tb;
         DUT.datapath_inst.memory_unit.memory[9'h006] = 32'h8A800087; // ldi R5, 0x87
         
         // --- Branching Test 1 ---
-        // brmi: Opcode 10101, C2 = --11 (3). 
         DUT.datapath_inst.memory_unit.memory[9'h007] = 32'hAA980003; // brmi R5, 3 (Will not branch)
         DUT.datapath_inst.memory_unit.memory[9'h008] = 32'h8AA80005; // ldi R5, 5(R5)
-        DUT.datapath_inst.memory_unit.memory[9'h009] = 32'h80A7FFFD; // ld  R1, -3(R5)
+        DUT.datapath_inst.memory_unit.memory[9'h009] = 32'h80AFFFFD; // ld  R1, -3(R5)
         DUT.datapath_inst.memory_unit.memory[9'h00A] = 32'hD0000000; // nop
         
         // --- Branching Test 2 ---
-        // brpl: Opcode 10101, C2 = --10 (2). 
         DUT.datapath_inst.memory_unit.memory[9'h00B] = 32'hA8900002; // brpl R1, 2 (Will branch to target)
         DUT.datapath_inst.memory_unit.memory[9'h00C] = 32'h89A80007; // ldi R3, 7(R5) (Skipped)
         DUT.datapath_inst.memory_unit.memory[9'h00D] = 32'h8B9FFFFC; // ldi R7, -4(R3) (Skipped)
@@ -58,8 +56,8 @@ module Phase3_tb;
         DUT.datapath_inst.memory_unit.memory[9'h00E] = 32'h03A90000; // add  R7, R5, R2
         DUT.datapath_inst.memory_unit.memory[9'h00F] = 32'h48880003; // addi R1, R1, 3
         DUT.datapath_inst.memory_unit.memory[9'h010] = 32'h70880000; // neg  R1, R1
-        DUT.datapath_inst.memory_unit.memory[9'h011] = 32'h78880000; // not  R1, R1
-        DUT.datapath_inst.memory_unit.memory[9'h012] = 32'h50880000; // andi R1, R1, 0xF
+        DUT.datapath_inst.memory_unit.memory[9'h011] = 32'h78880000; // not  R1, R1\
+        DUT.datapath_inst.memory_unit.memory[9'h012] = 32'h5088000F; // andi R1, R1, 0xF
         DUT.datapath_inst.memory_unit.memory[9'h013] = 32'h3A010000; // ror  R4, R0, R2
         DUT.datapath_inst.memory_unit.memory[9'h014] = 32'h58A00005; // ori  R1, R4, 5
         DUT.datapath_inst.memory_unit.memory[9'h015] = 32'h2A090000; // shra R4, R1, R2
